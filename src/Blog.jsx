db2431eb662b0b2479fd8f3a1cebfd7d3483ea80
grygoriev <vovagrigoriev@gmail.com>
2024-12-26 12:13:38 +0200

    Реализовать основной компонент с роутингом

---
+++ src/Blog.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { Route, Routes } from 'react-router-dom';
+
+const Content = styled.div`
+	padding: 120px 0;
+`;
+
+const H2 = styled.h2`
+	text-align: center;
+`;
+
+const Header = () => {
+	return (
+		<div>
+			<H2>Header</H2>
+		</div>
+	);
+};
+
+const Footer = () => {
+	return (
+		<div>
+			<H2>Footer</H2>
+		</div>
+	);
+};
+
+export const Blog = () => {
+	return (
+		<>
+			<Header />
+			<Content>
+				<H2>Контент страницы</H2>
+				<Routes>
+					<Route path="/" element={<div>Главная страница</div>} />
+					<Route path="/login" element={<div>Авторизация</div>} />
+					<Route path="/register" element={<div>Регистрация</div>} />
+					<Route path="/users" element={<div>Пользователи</div>} />
+					<Route path="/post" element={<div>Новая статья</div>} />
+					<Route path="/post/:postId" element={<div>Статья</div>} />
+					<Route path="*" element={<div>Ошибка</div>} />
+				</Routes>
+			</Content>
+			<Footer />
+		</>
+	);
+};
