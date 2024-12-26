ce91cd28b1f62c9d3d36cb77da36d3e328a3fcbb
grygoriev <vovagrigoriev@gmail.com>
2024-12-26 13:17:00 +0200

    Реализовать шапка и логотип

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 import { Route, Routes } from 'react-router-dom';
+import { Header } from './components';
+
+const AppColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 1000px;
+	min-height: 100%;
+	margin: 0 auto;
+	background-color: #fff;
+`;
 
 const Content = styled.div`
 	padding: 120px 0;
@@ -8,14 +19,6 @@ const Content = styled.div`
 const H2 = styled.h2`
 	text-align: center;
 `;
-
-const Header = () => {
-	return (
-		<div>
-			<H2>Header</H2>
-		</div>
-	);
-};
 
 const Footer = () => {
 	return (
@@ -27,7 +30,7 @@ const Footer = () => {
 
 export const Blog = () => {
 	return (
-		<>
+		<AppColumn>
 			<Header />
 			<Content>
 				<H2>Контент страницы</H2>
@@ -42,6 +45,6 @@ export const Blog = () => {
 				</Routes>
 			</Content>
 			<Footer />
-		</>
+		</AppColumn>
 	);
 };
